94b388786d2c9cbdb6db742a27c6754983089780
Gubato <luis_d_gutierrez@hotmail.com>
2025-01-28 08:28:18 -0600

    Cambios en los ejes de los gráficos

--- app.py
+++ app.py
@@ -14,11 +14,11 @@ button_disp = st.button('Generar grafico de dispersion')
 if button_hist:
 
     st.write('Generamos histograma a partir de los datos')
-    hist = px.histogram(df, x='condition')
+    hist = px.histogram(df, x='odometer')
     st.plotly_chart(hist, use_container_width=True)
 
 if button_disp:
     
     st.write('Generamos un grafico de dispersion a partir de los datos')
-    disp = px.scatter(df,x='model', y='condition')
+    disp = px.scatter(df,x='odometer', y='price')
     st.plotly_chart(disp,use_container_width=True)
